f39bc355e60c03378b1f048801117baa18c7bf14
fc01 <fc01@fookcode.com>
2016-02-15 16:46:09 +0800

    init 0.0.1

---
+++ fchrm/src/main/java/com/fookcode/fchrm/App.java
@@ -0,0 +1,13 @@
+package com.fookcode.fchrm;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
