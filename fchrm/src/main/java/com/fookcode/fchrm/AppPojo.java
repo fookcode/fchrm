e37721db16d303f74d9b3473f0af3d78cd3fbfbf
fookcode <fookcode@163.com>
2016-05-05 10:01:01 +0800

    5.5

---
+++ fchrm/src/main/java/com/fookcode/fchrm/AppPojo.java
@@ -0,0 +1,35 @@
+package com.fookcode.fchrm;
+
+public class AppPojo {
+	
+	public String name;
+	
+	public int age;
+	
+	public boolean sex;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public boolean isSex() {
+		return sex;
+	}
+
+	public void setSex(boolean sex) {
+		this.sex = sex;
+	}
+	
+}
